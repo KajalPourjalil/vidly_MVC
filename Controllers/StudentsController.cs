24afae07a84952007a8f4eba19807d71aeda3590
KajalPourjalil <rina.skyler@yahoo.com>
2024-02-09 21:56:34 +0300

    student controller created

---
+++ Controllers/StudentsController.cs
@@ -0,0 +1,13 @@
+using Microsoft.AspNetCore.Mvc;
+
+namespace StudentPortal.Web.Controllers
+{
+    public class StudentsController : Controller
+    {
+        [HttpGet]
+        public IActionResult Add()
+        {
+            return View();
+        }
+    }
+}
