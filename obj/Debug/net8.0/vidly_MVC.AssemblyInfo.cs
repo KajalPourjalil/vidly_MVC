b163e093cb8441ef8fdd14d368a1113e366564fc
KajalPourjalil <rina.skyler@yahoo.com>
2024-02-09 22:25:32 +0300

    launch.json and task.json file created

--- obj/Debug/net8.0/vidly_MVC.AssemblyInfo.cs
+++ obj/Debug/net8.0/vidly_MVC.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("vidly_MVC")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+24afae07a84952007a8f4eba19807d71aeda3590")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+bc20d164045ae9966f1544674c4dde04b6883d50")]
 [assembly: System.Reflection.AssemblyProductAttribute("vidly_MVC")]
 [assembly: System.Reflection.AssemblyTitleAttribute("vidly_MVC")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
