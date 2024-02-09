6a4b542cc08b5b3f01149743bc9a7e3e74ad9290
KajalPourjalil <rina.skyler@yahoo.com>
2024-02-09 21:51:17 +0300

    server name adjusted. sql server connected. table created

--- obj/Debug/net8.0/vidly_MVC.AssemblyInfo.cs
+++ obj/Debug/net8.0/vidly_MVC.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("vidly_MVC")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+15219d365d0b0148f4aaeaa55d76745b7749f5a0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7fd084994fa014981f5bf4925f539d86ce056ff6")]
 [assembly: System.Reflection.AssemblyProductAttribute("vidly_MVC")]
 [assembly: System.Reflection.AssemblyTitleAttribute("vidly_MVC")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
