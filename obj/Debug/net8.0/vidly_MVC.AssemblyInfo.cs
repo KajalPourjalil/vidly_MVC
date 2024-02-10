2995c0c0eff30d496d39d0ad681b4bfa1babd104
KajalPourjalil <rina.skyler@yahoo.com>
2024-02-10 21:46:23 +0300

    changed theme

--- obj/Debug/net8.0/vidly_MVC.AssemblyInfo.cs
+++ obj/Debug/net8.0/vidly_MVC.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("vidly_MVC")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b65343dffe29d0251ba3565e04ce5b6f0c48b96e")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+32781314c817767cdaa65ec72f6d800cc8107b42")]
 [assembly: System.Reflection.AssemblyProductAttribute("vidly_MVC")]
 [assembly: System.Reflection.AssemblyTitleAttribute("vidly_MVC")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
